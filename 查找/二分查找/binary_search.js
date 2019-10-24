888d067fb0aa6625b3aa227dadc2f93a85092c5c
LiMingjie_Office <limingjie_mail@163.com>
2019-10-24 16:11:01 +0800

    comiit

---
+++ 查找/二分查找/binary_search.js
@@ -0,0 +1,25 @@
+function binary_search(list, value) {
+  var low = 0;
+  var high = list.length - 1;
+
+  while (low <= high) {
+    mid = parseInt((low + high) / 2);
+    const guess = list[mid];
+    if (guess === value) {
+      return mid;
+    } else if (guess < value) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return null;
+}
+
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const value = 3;
+console.log("result:", binary_search(list, value));
+
+/*
+  $ node ./查找/二分查找/binary_search.js
+ */
