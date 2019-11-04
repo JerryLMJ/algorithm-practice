c768d2a39529cbfa66a528a61dad3e44077872a2
LiMingjie_Office <limingjie_mail@163.com>
2019-11-04 12:22:10 +0800

    commit

--- 排序/快速排序/quick_sort.c
+++ 排序/快速排序/quick_sort.c
@@ -1,53 +1,35 @@
 #include <stdio.h>
 
-int *quickSort(int arr[], int count)
+void swap(int arr[], int i, int j)
 {
-  if (count <= 1)
-  {
-    return arr;
-  }
-  else
-  {
-    int pivot = arr[0];
-    int less[] = {};
-    int lessIndes = 0;
-    int greater[] = {};
-    int greaterIndex = 0;
-    for (int i = 1; i < count; i++)
-    {
-      int el = arr[i];
-      if (el <= pivot)
-      {
-        less[lessIndes] = el;
-        lessIndes++;
-      }
-      else
-      {
-        greater[greaterIndex] = el;
-        greaterIndex++;
-      }
-    }
-    int *lessResult = quickSort(less, lessIndes + 1);
-    int *greaterResult = quickSort(greater, greaterIndex + 1);
-    int result[] = {};
-    for (int i = 0; i < lessIndes + 1; i++)
-    {
-      result[i] = lessResult[i];
-    }
-    result[lessIndes + 1] = pivot;
-    for (int i = 0; i < greaterIndex + 1; i++)
-    {
-      result[lessIndes + 1 + i] = greaterResult[i];
-    }
+  int temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
 
-    return *result;
+void quickSort(int arr[], int low, int high)
+{
+  if (low < high)
+  {
+    int last = low;
+    for (int i = low + 1; i <= high; i++)
+    {
+      if (arr[i] < arr[last])
+      {
+        swap(arr, i, last + 1);
+        swap(arr, last, last + 1);
+        last++;
+      }
+    }
+    quickSort(arr, low, last - 1);
+    quickSort(arr, last + 1, high);
   }
 }
 
 int main()
 {
   int arr[7] = {6, 2, 1, 8, 7, 3, 5};
-  quickSort(arr, 7);
+  quickSort(arr, 0, 6);
   printf("result:");
   for (int i = 0; i < 7; i++)
   {
